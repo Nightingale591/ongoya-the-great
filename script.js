35bdb420d5d7bca7196a556b85c111f54c329637
Nightingale591 <owengilbert591@gmail.com>
2026-01-22 15:18:11 +0300

    Add scripts

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Ongoya The Great loaded");
 
+  const form = document.getElementById("orderForm");
+  if (!form) return;
+
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+
+    const amountInput = document.querySelector("input[type='number']");
+    const amount = Number(amountInput.value);
+
+    const points = Math.floor(amount / 100);
+
+    const pointsBox = document.getElementById("points");
+    pointsBox.innerText = "Your Points: " + points;
+
+    alert("Order placed! You earned " + points + " loyalty points ⭐");
+  });
+});
